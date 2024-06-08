c532eab270dc3275c210656825c17312b3de8ad4
21NOMAD12 <toleevedilbek75@gmail.com>
2024-06-09 02:44:55 +0500

    Add files via upload

---
+++ src/main/java/org/example/musicfx/MusicList.java
@@ -0,0 +1,21 @@
+package org.example.musicfx;
+
+import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MusicList {
+    static String fileMSC = "music_list/";
+    static File[] file = new File[]{new File(fileMSC + "Svardstal_-_NIGHT_DRIVE.wav"),
+            new File(fileMSC + "LXST_CXNTURY_-_Andromeda.wav"),
+            new File(fileMSC + "MAXPVNK-VEM_-_slowed-reverb.wav"),
+            new File(fileMSC + "MONTAGEM-DO-COSMOS-_slowed-reverb.wav"),
+            new File(fileMSC + "ZXNTURY_JXXPSINNXR_FXLLEN_WXRRIOR_-_Farewell.wav"),
+            new File(fileMSC + "DVRST-Reason-to-Live-_slowed-_-reverb.wav"),
+            new File(fileMSC + "Shadow-Onimxru-_-Smithmane-_slowed-reverb.wav"),
+            new File(fileMSC + "smolov-SKY-rmx-Eternxlkz-SLAY-Slowed-Reverb.wav"),
+            new File(fileMSC + "SXULTAPE-VISION-LOVE-_slowed-reverb.wav"),
+            new File(fileMSC + "SXULTAPE-VISION-SUMMER-_Slowed-_-Reverb.wav"),
+            new File(fileMSC + "TheGoldenYami-KUTE_-OBLXKQ-DREAMCORE-_slowed-reverb.wav"),
+            new File(fileMSC + "yellow-pixie-night-finding-_slowed-reverb.wav"),
+            new File(fileMSC + "PASTEL-GHOST-Silhouette.wav")};
+}
