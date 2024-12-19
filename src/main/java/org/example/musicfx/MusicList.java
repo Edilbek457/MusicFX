17243b4b1d632f4a499af6e5979fed23c6093d16
Edilbek457 <toleevedilbek@gmail.com>
2024-12-19 23:00:18 +0500

    add 1 music

--- src/main/java/org/example/musicfx/MusicList.java
+++ src/main/java/org/example/musicfx/MusicList.java
@@ -5,21 +5,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MusicList {
     static String fileMSC = "music_list/";
-    static File[] file = new File[]{new File(fileMSC + "Svardstal_-_NIGHT_DRIVE.wav"),
-            new File(fileMSC + "LXST_CXNTURY_-_Andromeda.wav"),
-            new File(fileMSC + "MAXPVNK-VEM_-_slowed-reverb.wav"),
-            new File(fileMSC + "MONTAGEM-DO-COSMOS-_slowed-reverb.wav"),
-            new File(fileMSC + "ZXNTURY_JXXPSINNXR_FXLLEN_WXRRIOR_-_Farewell.wav"),
+    static File[] file = new File[]{
+            new File(fileMSC + "DRKHVN-SWERVE-slowed-reverb.wav"),
             new File(fileMSC + "DVRST-Reason-to-Live-_slowed-_-reverb.wav"),
+            new File(fileMSC + "ENXK-EUPHORIA-_slowed-reverb.wav"),
+            new File(fileMSC + "LXRY-PXNK-DESOLATE-_slowed-reverb_.mp4_1.wav"),
+            new File(fileMSC + "LXST_CXNTURY_-_Andromeda.wav"),
+            new File(fileMSC + "MONTAGEM-DO-COSMOS-_slowed-reverb.wav"),
+            new File(fileMSC + "PASTEL-GHOST-Silhouette.wav"),
             new File(fileMSC + "Shadow-Onimxru-_-Smithmane-_slowed-reverb.wav"),
             new File(fileMSC + "smolov-SKY-rmx-Eternxlkz-SLAY-Slowed-Reverb.wav"),
+            new File(fileMSC + "Svardstal_-_NIGHT_DRIVE.wav"),
             new File(fileMSC + "SXULTAPE-VISION-LOVE-_slowed-reverb.wav"),
             new File(fileMSC + "SXULTAPE-VISION-SUMMER-_Slowed-_-Reverb.wav"),
             new File(fileMSC + "TheGoldenYami-KUTE_-OBLXKQ-DREAMCORE-_slowed-reverb.wav"),
+            new File(fileMSC + "WAVEMANE - MIDNIGHT (slowed + reverb).wav"),
+            new File(fileMSC + "XXDED-i-like-the-way-you-kiss-me-_slowed-reverb_.wav"),
             new File(fileMSC + "yellow-pixie-night-finding-_slowed-reverb.wav"),
-            new File(fileMSC + "DRKHVN-SWERVE-slowed-reverb.wav"),
-            new File(fileMSC + "ENXK-EUPHORIA-_slowed-reverb.wav"),
-            new File(fileMSC + "LXRY-PXNK-DESOLATE-_slowed-reverb_.mp4_1.wav"),
-            new File(fileMSC + " XXDED-i-like-the-way-you-kiss-me-_slowed-reverb_.wav"),
-            new File(fileMSC + "PASTEL-GHOST-Silhouette.wav")};
+            new File(fileMSC + "ZXNTURY_JXXPSINNXR_FXLLEN_WXRRIOR_-_Farewell.wav")};
 }
